1440d1584416fa6d8c92d49724a65a6e26c83eb4
Jose Gomez <jose.fgm1995@gmail.com>
2018-11-08 14:55:27 -0500

    fixed bug in responsive buttons and scss structure created

--- ChallengeC05/Scripts/script.js
+++ ChallengeC05/Scripts/script.js
@@ -1,7 +1,10 @@
+const m = document.getElementById('midle-header');
+const u = document.getElementById('user-header');
+const n = document.getElementById('nav-container');
+
 document.getElementById('user').addEventListener('click', () => {
-  const m = document.getElementById('midle-header');
-  const u = document.getElementById('user-header');
   if (m.className === 'midle-header') {
+    n.className = 'nav-container';
     m.className += ' show-header';
     u.className += ' show-header';
   } else {
@@ -11,8 +14,9 @@ document.getElementById('user').addEventListener('click', () => {
 });
 
 document.getElementById('menu').addEventListener('click', () => {
-  const n = document.getElementById('nav-container');
   if (n.className === 'nav-container') {
+    m.className = 'midle-header';
+    u.className = 'user-header';
     n.className += ' show-nav';
   } else {
     n.className = 'nav-container';
